6ad89ceded7a5a71be767541ba298e2b25487b7b
Alain <alain.riou@ens-paris-saclay.fr>
2019-11-20 13:57:16 +0100

    use torchvision resnet

--- model.py
+++ model.py
@@ -2,20 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import torchvision
+
 n_classes = 20
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.resnet = torch.hub.load('facebookresearch/WSL-Images', 'resnext101_32x48d_wsl')
-        self.fc1 = nn.Linear(1000, 512)
-        self.fc2 = nn.Linear(512, n_classes)
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
+        self.resnet = torchvision.models.resnext101_32x8d(pretrained=True)
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+        self.resnet.fc = nn.Linear(512, n_classes)
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
         x = self.resnet(x)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
         return self.softmax(x)
